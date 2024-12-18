9318786a34085e2cc51cab1082ef06ce98adcf3e
eveeey <evelynovii@gmail.com>
2024-12-18 09:30:05 +0100

    Add project files.

---
+++ FørstWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FørstWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
